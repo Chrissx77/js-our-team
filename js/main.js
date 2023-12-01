4807899bdff4c90b8f9f7876da9bfc19fabd5d8e
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-01 16:55:26 +0100

    added creation arrayTeam with objects on js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const arrayTeam = [
+    {
+        "Name": "Wayne",
+        "Surname": "Barnett",
+        "Role": "Founder & CEO",
+        "Image": "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        "Name": "Angela",
+        "Surname": "Caroll",
+        "Role": "Chief Editor",
+        "Image": "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        "Name": "Walter",
+        "Surname": "Gordon",
+        "Role": "Office Manager",
+        "Image": "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        "Name": "Angela",
+        "Surname": "Lopez",
+        "Role": "Social Media Manager",
+        "Image": "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        "Name": "Scott",
+        "Surname": "Estrada",
+        "Role": "Developer",
+        "Image": "scott-estrada-developer"
+    },
+
+    {
+        "Name": "Barbara",
+        "Surname": "Ramos",
+        "Role": "Graphic Designer",
+        "Image": "wayne-barnett-founder-ceo.jpg"
+    }
+]
+
+console.log(arrayTeam);
+
+for(let i = 0; i < arrayTeam.length; i++){
+    let nome = arrayTeam[i].Name;
+    let ruolo = arrayTeam[i].Role;
+    let foto = arrayTeam[i].Image;
+    
+}
